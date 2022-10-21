6fc4e38c3c0c89c7ba47b360c66b597348676b0c
Precious <Preshomondi@gmail.com>
2022-10-22 02:31:58 +0300

    create EndButton components

---
+++ src/components/EndButton.js
@@ -0,0 +1,15 @@
+import React from "react"
+import "./EndButton.css"
+
+function EndButton(){
+    return(
+        <div className="end-buttons">
+        <button>Register</button>
+        <button>Explore</button>
+        <button>Buy</button>
+        <button>Share</button>
+    </div>
+    )
+}
+
+export default EndButton
