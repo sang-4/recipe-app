4c928a02aba4e6fed5d30bd2fabf83f235589792
Precious <Preshomondi@gmail.com>
2022-10-23 17:19:22 +0300

    remove conflict

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,62 @@
-import React from "react"
-import "./Footer.css"
+import React from "react";
+import { Link } from "react-router-dom";
 
-function Footer() {
-    return (
-        <div className="footer">
-            <div className="footer-text">
-                <div className="footer-one">
-                    <p>Privacy Policy</p>
-                </div>
-                <div className="footer-two">
-                    <p>Copyright@2022</p>
-                </div>
+const Footer = () => {
+  return (
+    <div>
+      <footer className="footer">
+        <div className="row">
+          <div className="col-lg-2">
+            <h1 className="greatVibes fw-bold">ReCipe</h1>
+          </div>
+          <div className="col-lg-3">
+            <div className="footer__Links">
+              <h2>Extras</h2>
+              <Link className="footer__Link" to="">
+                Our Products
+              </Link>
+              <Link className="footer__Link" to="">
+                News
+              </Link>
+              <Link className="footer__Link" to="">
+                Blog
+              </Link>
+              <Link className="footer__Link" to="">
+                Join Us
+              </Link>
             </div>
-            <div className="footer-line">
-                
+          </div>
+          <div className="col-lg-3">
+            <div className="contact">
+              <h2>Contacts</h2>
+              <p className="footer__Address">Address: 21070, 00100</p>
+              <p>
+                Phone: <a href="tel:+254799820376">+254799820376</a>
+              </p>
+              <p>
+                Gmail: <a href="mailto:Recipe@Gmail.Com">Recipe@Gmail.Com</a>
+              </p>
             </div>
-
-
+          </div>
+          <div className="col-lg-4">
+            <h2>Subscription</h2>
+            <div className="input__email mt-3">
+              <span className="me-1">Email:</span>
+              <input type="email" name="" id="" />
+            </div>
+            <div className="footer__Btn">
+              <button type="submit">Submit</button>
+            </div>
+          </div>
         </div>
-    )
-}
+      </footer>
 
-export default Footer
+      <div className="bottom__Footer">
+        <h5>Privacy Policy</h5>
+        <h5>CopyRight &#64; 2022</h5>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
