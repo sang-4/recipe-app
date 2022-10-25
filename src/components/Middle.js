655cbbd95baee419a68687f185f304e20678a749
Douglas <douglasobara97@gmail.com>
2022-10-25 09:42:57 +0300

    refactor precious code

--- src/components/Middle.js
+++ src/components/Middle.js
@@ -1,35 +1,55 @@
-import React from "react"
+import React from "react";
 
+function Middle() {
+  const WeeklyRecipes = [
+    {
+      id: 1,
+      title: "Chicken biryani",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullamsodales, sem a pretium ullamcorper, lorem lectus auctor ligula,semper mattis velit purus vel urna. Duis porta felis ac semposuere convallis.",
+      imageUrl:
+        "https://images.pexels.com/photos/7353380/pexels-photo-7353380.jpeg?auto=compress&cs=tinysrgb&w=400",
+    },
+    {
+      id: 2,
+      title: "Chicken biryani",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullamsodales, sem a pretium ullamcorper, lorem lectus auctor ligula,semper mattis velit purus vel urna. Duis porta felis ac semposuere convallis.",
+      imageUrl:
+        "https://images.pexels.com/photos/7353380/pexels-photo-7353380.jpeg?auto=compress&cs=tinysrgb&w=400",
+    },
+    {
+      id: 3,
+      title: "Chicken biryani",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullamsodales, sem a pretium ullamcorper, lorem lectus auctor ligula,semper mattis velit purus vel urna. Duis porta felis ac semposuere convallis.",
+      imageUrl:
+        "https://images.pexels.com/photos/7353380/pexels-photo-7353380.jpeg?auto=compress&cs=tinysrgb&w=400",
+    },
+  ];
 
-function Middle(){
-    return(
-        <div className="middle">
-        <h2>Weekly Recipes</h2>
-        <div className="middle-container">
-            <div className="panel" style={{ backgroundImage: "url('https://images.pexels.com/photos/7353380/pexels-photo-7353380.jpeg?auto=compress&cs=tinysrgb&w=400')" }}>
-                <h3 className="heading">Chicken biryani</h3>
-                <div className="middle-desc">
-                    <h3>Chicken biryani</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sodales, sem a pretium ullamcorper, lorem lectus auctor ligula, semper mattis velit purus vel urna. Duis porta felis ac sem posuere convallis. </p>
-                </div>
+  return (
+    <div className="middle">
+      <h2>Weekly Recipes</h2>
+      <div className="middle-container">
+        {WeeklyRecipes.map(({ id, title, content, imageUrl }) => (
+          <div
+            className="panel"
+            style={{
+              backgroundImage: `url(${imageUrl})`,
+            }}
+            key={id}
+          >
+            <h3 className="heading">{title}</h3>
+            <div className="middle-desc">
+              <h3>{title}</h3>
+              <p>{content}</p>
             </div>
-            <div className="panel" style={{ backgroundImage: "url('https://images.pexels.com/photos/7353380/pexels-photo-7353380.jpeg?auto=compress&cs=tinysrgb&w=400')" }}>
-                <h3 className="heading">Chicken biryani</h3>
-                <div className="middle-desc">
-                    <h3>Chicken biryani</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sodales, sem a pretium ullamcorper, lorem lectus auctor ligula, semper mattis velit purus vel urna. Duis porta felis ac sem posuere convallis. </p>
-                </div>
-            </div>
-            <div className="panel" style={{ backgroundImage: "url('https://images.pexels.com/photos/7353380/pexels-photo-7353380.jpeg?auto=compress&cs=tinysrgb&w=400')" }}>
-                <h3 className="heading">Chicken biryani</h3>
-                <div className="middle-desc">
-                    <h3>Chicken Biryani</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sodales, sem a pretium ullamcorper, lorem lectus auctor ligula, semper mattis velit purus vel urna. Duis porta felis ac sem posuere convallis. </p>
-                </div>
-            </div>
-        </div>
+          </div>
+        ))}
+      </div>
     </div>
-    )
+  );
 }
 
-export default Middle
+export default Middle;
