201b4894cd7f85888c902e4e6da77ca8b5a9a677
Precious <Preshomondi@gmail.com>
2022-10-28 23:49:15 +0300

    style Procedure component

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import Routes from "../src/routes/index"
 import Container from "react-bootstrap/Container";
 
 
+
 function App() {
   return (
+    
     <Container className="container__recipe">
       <Routes />
     </Container>
--- src/pages/SingleRecipe.js
+++ src/pages/SingleRecipe.js
@@ -27,7 +27,7 @@ const SingleRecipe = () => {
           <p>all connections to the npm registry - including for package installation - must use TLS 1.2 or higher. </p>
         </div>
         <div className="right-selectors">
-          <p>Ingredients</p>
+          <p className="new">Ingredients</p>
           <p>Procedure</p>
         </div>
       </div>
@@ -52,17 +52,7 @@ const SingleRecipe = () => {
             <li>fresh lettuce leaves</li>
           </ul>
         </div>
-        <div className="bottom two">
-          <ol>
-            <li>In a large bowl, mix ground beef, onion powder, salt and pepper until just combined. Do not overmix, or your patties will be tough.</li>
-            <li>Divide into six portions and form patties, without pressing too hard. They should be uniform in thickness. Smooth out any cracks using your fingers. Make these right before you grill them, so they stay at room temperature.</li>
-            <li>Preheat your grill, grill pan or cast-iron skillet to high heat and add burger patties. If using a grill, cover with the lid.</li>
-            <li>Cook until the crust that forms on the bottom of the burger releases it from the pan or grate — about 2 minutes. Gently test, but don't flip it until it gets to this point. When burgers lift up easily, flip, add two slices of cheese to each, close lid if using a grill, and cook on the other side for another 2-3 minutes for medium to medium rare.</li>
-            <li>Remove burgers with a sturdy metal spatula and transfer to a plate. Allow to rest for several minutes, then transfer to buns.</li>
-            <li>Garnish as desired and serve immediately.</li>
-
-          </ol>
-        </div>
+       
       </div>
     </div>
   </div>;
