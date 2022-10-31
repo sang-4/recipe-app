8aa214902f72d39adb8d8d22df2855ec6e3009c0
Precious <Preshomondi@gmail.com>
2022-10-31 10:18:57 +0300

    update

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Routes from "./routes/MainRoutes";
 import Container from "react-bootstrap/Container";
 
 
+
 function App() {
   return (
     <Container className="container__recipe">
       <Routes />
     </Container>
+   
    
   );
 }
